8d6a7deffdde76ba70f7cb7210693fb2eceebaf9
Josh <31908788+truemiller@users.noreply.github.com>
2023-02-18 20:37:09 +0000

    clean about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,17 +3,15 @@ export const About = () => (
     className="flex p-5 py-40  bg-opacity-50 text-white shadow-xl"
     id="about"
   >
-    <div className="container mx-auto flex flex-row justify-between">
-      <div className="grid grid-cols-1 xl:grid-cols-2 gap-10">
-        <div className="">
-          <h2>About</h2>
-          <p>BOGA Studios is an NFT studio.</p>
-          <p>We innovate and create new collectible NFTs.</p>
-          <p>
-            Numerous collections will be launched, with varying quantity,
-            pricing, and themes.
-          </p>
-        </div>
+    <div className="container mx-auto text-center">
+      <div className="">
+        <h2>About</h2>
+        <p>BOGA Studios is an NFT studio.</p>
+        <p>We innovate and create new collectible NFTs.</p>
+        <p>
+          Numerous collections will be launched, with varying quantity, pricing,
+          and themes.
+        </p>
       </div>
     </div>
   </section>
