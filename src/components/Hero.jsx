1b634b8d8bc2a382ebb408bbd2cfbc3358a6b4c8
Josh <31908788+truemiller@users.noreply.github.com>
2023-02-18 20:37:17 +0000

    remove hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,13 @@
 export const Hero = () => {
   return (
     <div className="flex p-5 py-40 bg-opacity-50  shadow-xl">
-      <div className="container mx-auto">
-        <div className="grid grid-cols-2 gap-5">
-          <h1 className="my-auto">BOGA Studios</h1>
-          <img
-            src="https://www.thisiscolossal.com/wp-content/uploads/2018/04/agif2opt.gif"
-            alt=""
-          />
-        </div>
+      <div className="container mx-auto text-center">
+        <h1 className="my-auto">BOGA Studios</h1>
+        {/* <img
+          src="https://www.thisiscolossal.com/wp-content/uploads/2018/04/agif2opt.gif"
+          alt=""
+          className="mx-auto"
+        /> */}
       </div>
     </div>
   )
