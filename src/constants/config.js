abfc67442394a8f0f9965caef4e8b5e32819afed
Josh <31908788+truemiller@users.noreply.github.com>
2023-02-19 01:23:29 +0000

    remove dev config

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,9 +1,19 @@
 export const RPC = 'https://api.avax-test.network/ext/bc/C/rpc'
-export const CHAIN_ID = '0x' + (43113).toString(16)
-export const CHAIN_NAME = 'Avalanche FUJI'
-export const BLOCK_EXPLORER = 'https://testnet.snowtrace.io/'
+export const CHAIN_ID = '0x' + (1).toString(16)
+export const CHAIN_NAME = 'Ethereum'
+export const BLOCK_EXPLORER = 'https://etherscan.io/'
 
-export const CONTRACT = '0x8B56346A1E3cAB1E89b888044E751e63Ae527a60'
+export const CONTRACT = '0x'
 
 export const MINT_MAX = 200
 export const MINT_PRICE = 0.01
+
+// export const RPC = 'https://api.avax-test.network/ext/bc/C/rpc'
+// export const CHAIN_ID = '0x' + (43113).toString(16)
+// export const CHAIN_NAME = 'Avalanche FUJI'
+// export const BLOCK_EXPLORER = 'https://testnet.snowtrace.io/'
+
+// export const CONTRACT = '0x8B56346A1E3cAB1E89b888044E751e63Ae527a60'
+
+// export const MINT_MAX = 200
+// export const MINT_PRICE = 0.01
