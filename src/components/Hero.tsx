c08ffb32c046e1c366bea51926f3750bd5e55a17
Josh <31908788+truemiller@users.noreply.github.com>
2023-02-19 23:29:09 +0000

    hero update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,26 @@
-import { memo } from 'react'
+import { memo } from "react";
+import { FaAngleDown } from "react-icons/fa";
 
 export const Hero = memo(() => {
   return (
-    <div className="flex p-5 py-40 bg-opacity-50  shadow-xl">
-      <div className="container mx-auto text-center">
-        <h1 className="mb-40">BOGA Studios</h1>
-        <img
-          src="/img/gif/dogs.gif"
-          alt=""
-          width={400}
-          className={'rounded-full my-5 mx-auto'}
-        />
+    <>
+      <div className="flex p-5 py-80 bg-opacity-50  shadow-xl">
+        <div className="container mx-auto text-center">
+          <h1 className="mb-5">BOGA Studios</h1>
+          <span className="block mb-5">Presents</span>
+          <span className="text-5xl mb-5">DOGS</span>
+          <span className="block pt-5">
+            <FaAngleDown style={{ margin: "0 auto" }} />
+          </span>
+        </div>
       </div>
-    </div>
-  )
-})
+      <div className="grid grid-cols-3">
+        <img src="/img/dogs/4.png" alt="" />
+        <img src="/img/dogs/5.png" alt="" />
+        <img src="/img/dogs/3.png" alt="" />
+      </div>
+    </>
+  );
+});
 
-export default Hero
+export default Hero;
